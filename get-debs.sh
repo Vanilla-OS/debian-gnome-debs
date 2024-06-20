0b07e85464e27c4dd470c30cd86d40286aacf6be
K.B.Dharun Krishna <kbdharunkrishna@gmail.com>
2024-06-20 20:07:07 +0530

    feat: add workflow to fetch debs
    Signed-off-by: K.B.Dharun Krishna <kbdharunkrishna@gmail.com>

---
+++ get-debs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wget http://ftp.debian.org/debian/pool/main/g/gnome-shell/gnome-shell_46.2-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/m/mutter/mutter_46.2-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/m/mutter/mutter-common_46.2-1_all.deb
+wget http://ftp.debian.org/debian/pool/main/m/mutter/mutter-14-tests_46.2-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/m/mutter/libmutter-test-14_46.2-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/m/mutter/libmutter-14-dev_46.2-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/m/mutter/gir1.2-mutter-14_46.2-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/m/mutter/mutter-common-bin_46.2-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/m/mutter/libmutter-14-0_46.2-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/g/gnome-shell/gnome-shell-common_46.2-1_all.deb
+wget http://ftp.debian.org/debian/pool/main/g/gnome-shell/gnome-shell-extension-prefs_46.2-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/g/gnome-remote-desktop/gnome-remote-desktop_46.2-1_amd64.deb
